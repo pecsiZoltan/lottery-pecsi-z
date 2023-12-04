a1f94d3a00d6dda9a81759b84149284160e225b9
pecsi.zolan <pecsi.zoltan@progmasters.hu>
2023-12-04 07:03:29 +0100

    refact guard

--- lottery-frontend/src/app/guard/game.can-activate.ts
+++ lottery-frontend/src/app/guard/game.can-activate.ts
@@ -3,37 +3,19 @@ import {UserState} from "../store/state/user";
 import {inject} from "@angular/core";
 import {Store} from "@ngrx/store";
 import {selectRole} from "../store/selector/user.selector";
-import {LoginService} from "../service/login.service";
-import {logUserIn} from "../store/action/user.actions";
+import {map} from "rxjs";
 
-export const gameCanActivate: CanActivateFn = async () => {
+export const gameCanActivate: CanActivateFn = () => {
 
   const router: Router = inject(Router);
-  const loginService: LoginService = inject(LoginService);
   const store: Store<UserState> = inject(Store<UserState>);
 
-  let role: 'PLAYER' | 'GUEST' | null = null;
-
-  store.select(selectRole).subscribe(
-    value => {
-      console.log('gotRoleFromStore');
-      role = value;
-      if (role != 'PLAYER') {
-        'subscribing to me()'
-        loginService.me().subscribe(
-          user => {
-            value = user.role;
-            store.dispatch(
-              logUserIn({
-                  user: user
-                }
-              )
-            )
-          }
-        )
-      }
-    }
+  return store.select(selectRole).pipe(
+    map(
+      role => role === 'PLAYER'
+        ? true
+        : router.createUrlTree(['login'])
+    )
   )
-  console.log('guard returns');
-  return role === 'PLAYER' ? true : router.navigate(['/login']);
+
 }
